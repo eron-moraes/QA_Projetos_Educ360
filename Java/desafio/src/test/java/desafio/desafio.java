59c8952bd584b7ecaa0fe42a89ddca9ab267c2ce
Eron Moraes <eron.moraes7@gmail.com>
2023-08-29 22:49:19 -0300

    desafio selenium

---
+++ Java/desafio/src/test/java/desafio/desafio.java
@@ -0,0 +1,5 @@
+package desafio;
+
+public class desafio {
+
+}
--- Selenium/Edu360Selenium/src/test/java/Tests/Utils.java
+++ Selenium/Edu360Selenium/src/test/java/Tests/Utils.java
@@ -24,7 +24,6 @@ public class Utils {
     }
     
     @Test
-
     public void testLogin() {
         Login login = new Login(driver);
         login.acessarPaginaDeLogin();
