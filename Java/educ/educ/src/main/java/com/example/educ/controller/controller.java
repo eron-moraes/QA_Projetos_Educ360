d90714b2b0eba8a2909bed90ceb3d6ff139982fd
Eron Moraes <eron.moraes7@gmail.com>
2023-09-12 15:19:35 -0300

    ann

--- Java/educ/educ/src/main/java/com/example/educ/controller/controller.java
+++ Java/educ/educ/src/main/java/com/example/educ/controller/controller.java
@@ -1,14 +1,15 @@
 package com.example.educ.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class controller {
 
-	@GetMapping("/teste")
-	public String olaMundo() {
-		return "Ola Mundo";
+	@GetMapping("/msg/{nome}")
+	public String olaMundo(@PathVariable String nome) {
+		return "Ola " +nome;
 	}
 	
 }
