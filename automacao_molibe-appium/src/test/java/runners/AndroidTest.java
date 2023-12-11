2ac3d9eb32d7ebb7024ba7abdbb21d622d4db4bf
Eron Moraes <eron.moraes7@gmail.com>
2023-12-11 20:33:36 -0300

    educ

--- automacao_molibe-appium/src/test/java/runners/AndroidTest.java
+++ automacao_molibe-appium/src/test/java/runners/AndroidTest.java
@@ -1,18 +1,6 @@
 package runners;
 
-ns;
-import cucumber.api.junit.Cucumber
-
-.junit.runner.RunWith;
-
-import configuration.DriverFactory;
-import cucumber.api.CucumberOpt
-
 import java.net.MalformedURLException;
-
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import or;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"pretty", "html:target/cucumberHtmlReport"}, dryRun = false,
